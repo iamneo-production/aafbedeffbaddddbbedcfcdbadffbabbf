ae3865f2a28242b0a12ac794efb8754a16a5a3bf
Iamneo <techadmin@iamneo.ai>
2023-08-05 06:20:27 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.jsx
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -18,6 +18,31 @@ export default function Stopwatch(props) {
 
     const initiatePause = () => {
         clearInterval(increased.current);
-        setIsPaused()
-    }
+        setIsPaused(!isPaused);
+    };
+
+    const initiateResume = () => {
+        setIsPaused(!isPaused);
+        increased.current = setInterval(() => {
+            setTime((time) => time + 1000)
+        },1000)
+    };
+
+    const initiateReset = () => {
+        clearInterval(increased.current)
+        setIsActive(false)
+        setIsPaused(false)
+        setTime(0)
+    };
+
+    return(
+        <section id='stopwatch'>
+            <div className='inner'>
+                <h1> React Stopwatch </h1>
+                <p id='time' data-testid='time'>
+                    {`0${Math.floor(time%360000)}`}
+                </p>
+            </div>
+        </section>
+    )
 }
